bd7baf1605a0ad8286d302a4ae1a4b54c43b2ccb
Brian Danin <brian.danin@mindsing.com>
2024-10-07 16:49:04 -0700

    Remove empty line

--- tailwind/webpack.mix.js
+++ tailwind/webpack.mix.js
@@ -8,7 +8,6 @@ const cwv_theme_dist = 'web/themes/custom/cwv_theme/dist';
 
 mix.setPublicPath(path.normalize('../'));
 
-
 mix.postCss('./src/main.css', `${cwv_theme_dist}`)
   .postCss('./src/admin.css', `${cwv_theme_dist}`)
   .postCss('./src/critical.css', `${cwv_theme_dist}`)
